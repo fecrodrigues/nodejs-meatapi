dbe947edb23ea71702f52560ff3afedefdca3ed6
Felipe Cavalcante <felipe.cavalcante.rod@gmail.com>
2019-05-20 18:09:51 -0300

    deixando de retornar password no find de usuario , não expondo dados sensíveis

--- users/users.model.ts
+++ users/users.model.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   name: { type: String },
   email: { type: String, unique: true },
-  password: { type: String }
+  password: { type: String, select: false }
 });
 
 export const User = mongoose.model('User', userSchema);
