f963dab46bcb5decc7cc9bb1830401d0290730f7
Felipe Cavalcante <felipe.cavalcante.rod@gmail.com>
2019-05-21 12:25:41 -0300

    adicionado metodo patch e delete

--- users/users.router.ts
+++ users/users.router.ts
@@ -45,7 +45,7 @@ class UsersRouter extends Router {
       const options = { overwrite: true }
       User.update({ _id: req.params.id }, req.body, options, (err) => {
         if(!err) {
-          User.findOne({ _id: req.params.id }, (err, user) => {
+          User.findById({ _id: req.params.id }, (err, user) => {
             if(!err) {
               res.json({ message: 'User successfully replaced', user: user });
             }
@@ -59,6 +59,31 @@ class UsersRouter extends Router {
           return next();
         }
       });
+    });
+
+    application.patch('/users/:id', (req, res, next) => {
+      const options = { new: true };
+      User.findByIdAndUpdate(req.params.id, req.body, options, (err, user) => {
+        if(!err) {
+          res.json({ message: 'User successfully updated', user: user });
+        } else {
+          res.status(404);
+          res.json({ message: 'User not found', error: err.message || err.errmsg });
+        }
+
+        return next();
+      })
+    });
+
+    application.del('/users/:id', (req, res, next) => {
+      User.deleteOne({ _id: req.params.id }, (err) => {
+        if(!err) {
+          res.send(204);
+        } else {
+          res.status(404);
+          res.json({ message: 'User not found', error: err.message || err.errmsg });
+        }
+      })
     });
 
   }
